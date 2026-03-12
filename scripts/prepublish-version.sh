ae611ad57ec61c32fdf830dc35737ab57698a853
Kushuh <geoffroy.vincent@agorastoryverse.com>
2026-03-12 19:58:41 +0100

    hotfix(scripts): use correct sed replacement on macOS (darwin) systems

--- scripts/prepublish-version.sh
+++ scripts/prepublish-version.sh
@@ -5,12 +5,10 @@ set -e
 case "$OSTYPE" in
   darwin*|bsd*)
     echo "Using BSD sed style"
-    sed_no_backup=( -i '' )
+    sed -i '' -E "s|($1)v[0-9.]+|\1v$(node -p -e "require('./package.json').version")|g" $2
     ;;
   *)
     echo "Using GNU sed style"
-    sed_no_backup=( -i )
+    sed -i -E "s|\($1\)v[0-9.]+|\1v$(node -p -e "require('./package.json').version")|g" $2
     ;;
 esac
-
-sed ${sed_no_backup[@]} -E "s|($1)v[0-9.]+|\1v$(node -p -e "require('./package.json').version")|g" $2
